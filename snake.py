4bb8361f012ad261d4120bdee063770a36637fe1
Estevão Coelho Kiel de Oliveira <43081305+eckorandon@users.noreply.github.com>
2021-07-19 17:28:44 -0300

    Update snake.py

--- snake.py
+++ snake.py
@@ -12,10 +12,10 @@ black = (0, 0, 0)
 white = (255, 255, 255)
 yellow = (255, 255, 102)
 red = (255, 0, 0)
-green = (0, 255, 0)
+green = (0, 255, 51)
 blue = (50, 153, 213)
 
-# creations of display
+# creation of display
 dis_width = 600
 dis_height  = 400
 dis = pygame.display.set_mode((dis_width, dis_width))
